5429d41d11c14883841e6a10e8d6693ed8fba68d
Binggo <ghafranpq@gmail.com>
2026-02-01 17:32:35 +0700

    Initial Dell PC website

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("buyBtn");
+const message = document.getElementById("message");
+
+button.addEventListener("click", () => {
+    message.textContent = "Dell PCs are known for quality, support, and long-term reliability.";
+});
